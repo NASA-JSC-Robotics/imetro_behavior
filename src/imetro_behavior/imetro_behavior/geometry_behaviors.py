36d64579c7723fc95aa06b123657fd2d5c319d76
Connor <connor.m.rainen@nasa.gov>
2026-08-19 13:18:52 -0500

    initial commit, very rough

--- src/imetro_behavior/imetro_behavior/geometry_behaviors.py
+++ src/imetro_behavior/imetro_behavior/geometry_behaviors.py
@@ -278,6 +278,111 @@ class TwistAboutPose(BehaviourWithPorts):
         self._set_output("output_pose", output_pose)
         return Status.SUCCESS
 
+class GetRotationOfPoses(BehaviourWithPorts):
+    """Returns the Roll, Pitch, and Yaw from one pose orientation to another."""
+
+    @classmethod
+    def input_ports(cls) -> dict:
+        """Input ports for required poses to check rotation between.
+        The rotation will be relative to the reference pose.
+        Note: both poses must share the same parent frame."""
+        return {
+            "reference_pose": PortInformation(
+                data_type=PoseStamped, required=True, description="reference frame for which the output rpy."
+            ),
+            "target_pose": PortInformation(
+                data_type=PoseStamped,
+                required=True,
+                description="PoseStamped that will have its rotation checked.",
+            ),
+        }
+
+    @classmethod
+    def output_ports(cls) -> dict:
+        """Returns output_pose, a pose rotated about the given rotation pose and parented to reference frame."""
+        return {
+            "rotation_amount": PortInformation(
+                data_type=list[float],
+                required=True,
+                description="Roll, Pitch, and Yaw of the rotation from reference to target.",
+            )
+        }
+
+    def setup(self, **kwargs):
+        """Get access to the ROS node for logger."""
+        self.node = kwargs.get("node")
+        if not isinstance(self.node, Node):
+            raise KeyError(f"A valid ROS node is required to setup the '{self.qualified_name}' node.")
+
+    def update(self) -> Status:
+        """Twist about the rotation frame."""
+        rotation_posestamp = self.get_input("rotation_pose")
+        target_posestamp = self.get_input("target_pose")
+        rotation_amount = self.get_input("rotation_amount")
+        rotation_axis = self.get_input("rotation_axis")
+        keep_start_orientation = self.get_input("keep_start_orientation")
+
+        if rotation_posestamp.header.frame_id != target_posestamp.header.frame_id:
+            self.node.get_logger().error("Error: given input poses do not share the same reference frame.")
+            return Status.FAILURE
+
+        # Convert the inputs to 4x4 transformation matrices
+        rotation_orientation = R.from_quat(
+            [
+                rotation_posestamp.pose.orientation.x,
+                rotation_posestamp.pose.orientation.y,
+                rotation_posestamp.pose.orientation.z,
+                rotation_posestamp.pose.orientation.w,
+            ]
+        )
+        rotation_translation = np.array(
+            [rotation_posestamp.pose.position.x, rotation_posestamp.pose.position.y, rotation_posestamp.pose.position.z]
+        )
+        rotation_T_reference = RigidTransform.from_components(rotation_translation, rotation_orientation)
+        target_orientation = R.from_quat(
+            [
+                target_posestamp.pose.orientation.x,
+                target_posestamp.pose.orientation.y,
+                target_posestamp.pose.orientation.z,
+                target_posestamp.pose.orientation.w,
+            ]
+        )
+        target_translation = np.array(
+            [target_posestamp.pose.position.x, target_posestamp.pose.position.y, target_posestamp.pose.position.z]
+        )
+        target_T_reference = RigidTransform.from_components(target_translation, target_orientation)
+
+        target_T_rotation = rotation_T_reference.inv() * target_T_reference
+
+        # Rotate the EE about the rotation frame by the given axis and angle in radians
+        twist_vector = rotation_amount * np.array(rotation_axis)
+        twist = R.from_rotvec(twist_vector)
+        twist_matrix = RigidTransform.from_components(np.zeros(3), twist)
+
+        twistedtarget_T_rotation = twist_matrix * target_T_rotation
+
+        # Lastly we want to take the twisted point and put in respect to reference frame
+        twistedtarget_T_reference = rotation_T_reference * twistedtarget_T_rotation
+        final_pose = twistedtarget_T_reference.translation
+        final_rotation = twistedtarget_T_reference.rotation.as_quat()
+
+        # Output final message
+        output_pose = PoseStamped()
+        output_pose.header = rotation_posestamp.header
+        output_pose.pose.position.x = final_pose[0]
+        output_pose.pose.position.y = final_pose[1]
+        output_pose.pose.position.z = final_pose[2]
+        if keep_start_orientation:
+            output_pose.pose.orientation = target_posestamp.pose.orientation
+        else:
+            output_pose.pose.orientation.x = final_rotation[0]
+            output_pose.pose.orientation.y = final_rotation[1]
+            output_pose.pose.orientation.z = final_rotation[2]
+            output_pose.pose.orientation.w = final_rotation[3]
+
+        self._set_output("output_pose", output_pose)
+        return Status.SUCCESS
+
 
 class OffsetPoseStamped(BehaviourWithPorts):
     """Offset a PoseStamped ROS message based on input translation and rotation offsets."""
