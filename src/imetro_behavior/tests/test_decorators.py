a3180fd4094187e05db779dc5c7e237e4c9ef37c
Misha Savchenko <misha.savchenko@nasa.gov>
2026-08-25 18:12:08 -0500

    pared things down a bit

--- src/imetro_behavior/tests/test_decorators.py
+++ src/imetro_behavior/tests/test_decorators.py
@@ -93,104 +93,74 @@ def test_success_if_variable_is_false_ticks_child() -> None:
 
 def test_timeout() -> None:
     running = py_trees.behaviours.Running(name="Running")
+
     timeout = TimeoutPort(name="Timeout", child=running)
     timeout.setup_ports()
-
     Blackboard.set(timeout._get_blackboard_key("duration"), 0.2)
-
-    print(py_trees.display.unicode_tree(timeout))
-    visitor = py_trees.visitors.DebugVisitor()
 
     # Test that it times out and re-initialises properly
     for i in range(0, 2):
-        py_trees.tests.tick_tree(timeout, 2 * i + 1, 2 * i + 1, visitors=[visitor])
+        timeout.tick_once()
 
-        print("\n--------- Assertions ---------\n")
-        print("timeout.status == py_trees.common.Status.RUNNING")
-        assert timeout.status == py_trees.common.Status.RUNNING
-        print("running.status == py_trees.common.Status.RUNNING")
-        assert running.status == py_trees.common.Status.RUNNING
+        assert timeout.status == Status.RUNNING
+        assert running.status == Status.RUNNING
 
         time.sleep(0.3)
-        py_trees.tests.tick_tree(timeout, 2 * i + 2, 2 * i + 2, visitors=[visitor])
 
-        print("\n--------- Assertions ---------\n")
-        print("timeout.status == py_trees.common.Status.FAILURE")
-        assert timeout.status == py_trees.common.Status.FAILURE
-        print("running.status == py_trees.common.Status.INVALID")  # type: ignore[unreachable]
-        assert running.status == py_trees.common.Status.INVALID
+        timeout.tick_once()
+
+        assert timeout.status == Status.FAILURE
+        assert running.status == Status.INVALID
 
     # test that it passes on success
     count = py_trees.behaviours.StatusQueue(
         name="Queue",
-        queue=[py_trees.common.Status.RUNNING],
-        eventually=py_trees.common.Status.SUCCESS,
+        queue=[Status.RUNNING],
+        eventually=Status.SUCCESS,
     )
+
     timeout = TimeoutPort(name="Timeout", child=count)
     timeout.setup_ports()
-
     Blackboard.set(timeout._get_blackboard_key("duration"), 0.2)
 
-    print(py_trees.display.unicode_tree(timeout))
+    timeout.tick_once()
+    assert timeout.status == Status.RUNNING
+    assert count.status == Status.RUNNING
 
-    py_trees.tests.tick_tree(timeout, 1, 1, visitors=[visitor])
-
-    print("\n--------- Assertions ---------\n")
-    print("timeout.status == py_trees.common.Status.RUNNING")
-    assert timeout.status == py_trees.common.Status.RUNNING
-    print("count.status == py_trees.common.Status.RUNNING")
-    assert count.status == py_trees.common.Status.RUNNING
-
-    py_trees.tests.tick_tree(timeout, 2, 2, visitors=[visitor])
-
-    print("\n--------- Assertions ---------\n")
-    print("timeout.status == py_trees.common.Status.SUCCESS")
-    assert timeout.status == py_trees.common.Status.SUCCESS
-    print("count.status == py_trees.common.Status.SUCCESS")  # type: ignore[unreachable]
-    assert count.status == py_trees.common.Status.SUCCESS
+    timeout.tick_once()
+    assert timeout.status == Status.SUCCESS
+    assert count.status == Status.SUCCESS
 
     # test that it passes on failure
     failure = py_trees.behaviours.Failure(name="Failure")
+
     timeout = TimeoutPort(name="Timeout", child=failure)
     timeout.setup_ports()
-
     Blackboard.set(timeout._get_blackboard_key("duration"), 0.2)
 
-    print(py_trees.display.unicode_tree(timeout))
+    timeout.tick_once()
 
-    py_trees.tests.tick_tree(timeout, 1, 1, visitors=[visitor])
-
-    print("\n--------- Assertions ---------\n")
-    print("timeout.status == py_trees.common.Status.FAILURE")
-    assert timeout.status == py_trees.common.Status.FAILURE
-    print("failure.status == py_trees.common.Status.FAILURE")
-    assert failure.status == py_trees.common.Status.FAILURE
+    assert timeout.status == Status.FAILURE
+    assert failure.status == Status.FAILURE
 
     # test that it succeeds if child succeeds on last tick
     count = py_trees.behaviours.StatusQueue(
         name="Queue",
-        queue=[py_trees.common.Status.RUNNING],
-        eventually=py_trees.common.Status.SUCCESS,
+        queue=[Status.RUNNING],
+        eventually=Status.SUCCESS,
     )
     timeout = TimeoutPort(name="Timeout", child=count)
     timeout.setup_ports()
 
     Blackboard.set(timeout._get_blackboard_key("duration"), 0.1)
-    print(py_trees.display.unicode_tree(timeout))
 
-    py_trees.tests.tick_tree(timeout, 1, 1, visitors=[visitor])
+    timeout.tick_once()
 
-    print("\n--------- Assertions ---------\n")
-    print("timeout.status == py_trees.common.Status.RUNNING")
-    assert timeout.status == py_trees.common.Status.RUNNING
-    print("count.status == py_trees.common.Status.RUNNING")
-    assert count.status == py_trees.common.Status.RUNNING
+    assert timeout.status == Status.RUNNING
+    assert count.status == Status.RUNNING
 
     time.sleep(0.2)  # go past the duration
-    py_trees.tests.tick_tree(timeout, 2, 2, visitors=[visitor])
+    timeout.tick_once()
 
-    print("\n--------- Assertions ---------\n")
-    print("timeout.status == py_trees.common.Status.SUCCESS")
-    assert timeout.status == py_trees.common.Status.SUCCESS
-    print("count.status == py_trees.common.Status.SUCCESS")
-    assert count.status == py_trees.common.Status.SUCCESS
+    assert timeout.status == Status.SUCCESS
+    assert count.status == Status.SUCCESS
