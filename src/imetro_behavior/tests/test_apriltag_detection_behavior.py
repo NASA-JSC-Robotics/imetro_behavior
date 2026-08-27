178c2d60b5be6db48818ca1d04f61aaff3f3ee33
Sebastian Castro <sebastian.a.castrofernandez@nasa.gov>
2026-08-27 16:31:43 -0400

    Patch pupil_apriltag shutdown bug

--- src/imetro_behavior/tests/test_apriltag_detection_behavior.py
+++ src/imetro_behavior/tests/test_apriltag_detection_behavior.py
@@ -50,7 +50,9 @@ def apriltag_behavior(ros_node: Node):
     Blackboard.set(behavior._get_blackboard_key("tag_size"), 0.073)
     Blackboard.set(behavior._get_blackboard_key("tag_family"), "tag36h11")
 
-    return behavior
+    yield behavior
+
+    behavior.shutdown()
 
 
 def test_detect_apriltag_success(apriltag_behavior) -> None:
