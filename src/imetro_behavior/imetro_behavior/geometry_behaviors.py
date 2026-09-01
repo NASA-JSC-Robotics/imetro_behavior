00e80bdf286b841ab1a7b17191f71f08639238b8
Connor <connor.m.rainen@nasa.gov>
2026-09-01 12:32:49 -0500

    minor tweak for efficiency

--- src/imetro_behavior/imetro_behavior/geometry_behaviors.py
+++ src/imetro_behavior/imetro_behavior/geometry_behaviors.py
@@ -95,7 +95,7 @@ class TransformPose(BehaviourWithPorts):
             return Status.FAILURE
 
         output_pose = PoseStamped()
-        output_pose.header = input_pose.header
+        output_pose.header.stamp = input_pose.header.stamp
         output_pose.header.frame_id = source_frame
         output_pose.pose = tf2_geometry_msgs.do_transform_pose(input_pose.pose, tform)
         self._set_output("output_pose", output_pose)
